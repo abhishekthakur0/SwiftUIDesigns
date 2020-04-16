462b7bd5f5905cd36020d974a934868fc5754eba
Abhishek Thakur <kumarcops@gmail.com>
2020-04-17 00:11:40 +0530

    Initial Commit

---
+++ SwiftUIDesigns/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDesigns
+//
+//  Created by Abhishek Thakur on 17/04/20.
+//  Copyright © 2020 Abhishek Thakur. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
