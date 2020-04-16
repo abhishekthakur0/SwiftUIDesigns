5ba7dfaf6cdd3d3d98b5526193d561bc7da19d6e
Abhishek Thakur <kumarcops@gmail.com>
2020-04-17 01:31:33 +0530

    Random

--- SwiftUIDesigns/ContentView.swift
+++ SwiftUIDesigns/ContentView.swift
@@ -9,8 +9,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var show = false
     var body: some View {
-        Text("Hello, World!")
+        
+//        VStack{
+//            Text("Hello, World!")
+//                .font(.largeTitle)
+//                .foregroundColor(Color.black)
+//            Text("A animation example")
+//                .font(.subheadline)
+//                .foregroundColor(.green)
+//                .scaleEffect(show ? 2: 1)
+//                .animation(.spring())
+//            Button(action:{
+//                withAnimation{
+//                    self.show.toggle()}
+//            }){
+//                Text("Animate")
+//                    .font(.body)
+//                    .foregroundColor(.red)
+//            }
+//        }
+        VStack {
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                VStack {
+                    Text("Animate")
+                        .font(.largeTitle)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color(hue: 0.39, saturation: 0.223, brightness: 0.896, opacity: 0.921))
+                }
+            .frame(width: 300, height: 300)
+                .background(Color.gray)
+            }
+            .cornerRadius(30)
+        }
+        
     }
 }
 
